b59bb62b87afb8e588c516526242fc437dc510da
skiden147 <135439001+skiden147@users.noreply.github.com>
2024-04-10 17:46:10 +0300

    Тесты переделаны

--- app/src/test/kotlin/ru/netology/testing/uiautomator/SampleTest.java
+++ app/src/test/kotlin/ru/netology/testing/uiautomator/SampleTest.java
@@ -8,6 +8,7 @@ import io.appium.java_client.remote.options.BaseOptions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.*;
 
@@ -17,19 +18,22 @@ import java.net.URL;
 public class SampleTest {
 
     private AndroidDriver driver;
+    private PORT = 4723;
+    private HOST = "127.0.0.1";
+
 
     @BeforeEach
     public void setUp() throws MalformedURLException {
-        var options = new BaseOptions()
-                .amend("platformName", "Android")
-                .amend("appium:deviceName", "Some name")
-                .amend("appium:appPackage", "ru.netology.testing.uiautomator")
-                .amend("appium:appActivity", ".MainActivity")
-                .amend("appium:automationName", "uiautomator2")
-                .amend("appium:ensureWebviewsHavePages", true)
-                .amend("appium:nativeWebScreenshot", true)
-                .amend("appium:newCommandTimeout", 3600)
-                .amend("appium:connectHardwareKeyboard", true);
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:deviceName", "Some name");
+        desiredCapabilities.setCapability("appium:appPackage", "ru.netology.testing.uiautomator");
+        desiredCapabilities.setCapability("appium:appActivity", ".MainActivity");
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
                 URL remoteURL = new URL("http://127.0.0.1:4723/");
 
                 try{
@@ -38,28 +42,31 @@ public class SampleTest {
                     throw new RuntimeException(e);
                 }
 
-                driver = new AndroidDriver(remoteURL, options);
+                driver = new AndroidDriver(remoteURL, desiredCapabilities);
+    }
+
+
+
+    @Test
+    public void testNoTextAdded() {
+        WebElement el3 = (WebElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
+        String oldText = el3.getText();
+        WebElement el1 = (WebElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
+        el1.sendKeys("  ");
+        WebElement el2 = (WebElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonChange");
+        el2.click();
+        Assertions.assertEquals(el3.getText() , oldText);
     }
 
     @Test
     public void testNewWindowText() {
-        var el1 = driver.findElement(AppiumBy.id("ru.netology.testing.uiautomator:id/userInput"));
-        el1.click();
-        el1.sendKeys("Netology");
-        var el2 = driver.findElement(AppiumBy.id("ru.netology.testing.uiautomator:id/buttonActivity"));
+        String textToSet = "Appium";
+        WebElement el1 = (WebElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
+        el1.sendKeys("Appium");
+        WebElement el2 = (WebElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonActivity");
         el2.click();
-        var el3 = driver.findElement(AppiumBy.id("ru.netology.testing.uiautomator:id/text"));
-        el3.isDisplayed();
-        Assert.assertEquals(el3.getText() , "Netology");
-    }
-
-    @Test
-    public void testNoTextAdded() {
-        var el1 = driver.findElement(AppiumBy.id("ru.netology.testing.uiautomator:id/buttonChange"));
-        el1.click();
-        var el2 = driver.findElement(AppiumBy.id("ru.netology.testing.uiautomator:id/textToBeChanged"));
-        el2.isDisplayed();
-        Assert.assertEquals(el2.getText() , "Hello UiAutomator!");
+        WebElement el3 = (WebElement) driver.findElementById("ru.netology.testing.uiautomator:id/text");
+        Assertions.assertEquals(el3.getText(), textToSet);
     }
 
     @AfterEach
